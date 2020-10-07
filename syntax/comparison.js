17520af6c242588df21c65391cd5144c961e9a16
rlaxodn0320 <48887569+rlaxodn0320@users.noreply.github.com>
2020-10-07 22:35:57 +0900

    Add files via upload

---
+++ syntax/comparison.js
@@ -0,0 +1,9 @@
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //true
+console.log(1===1); //true
+console.log(1===2); //false
+name = 1;
+console.log(name==1); //true
+console.log(name===1); //true
---
+++ syntax/variable.js
@@ -0,0 +1,6 @@
+var a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+//변수 = 상수;
+// 1 = 2;
