b74fbebba83022c41ac26194a9b0f60bb0acd2b6
rlaxodn0320 <48887569+rlaxodn0320@users.noreply.github.com>
2020-11-08 00:28:04 +0900

    Add files via upload

---
+++ syntax/sync.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+/*
+//readFileSync
+console.log('A');
+var result = fs.readFileSync('syntax/sample.txt', 'utf8');
+console.log(result);
+console.log('C');
+
+//ABC
+*/
+
+
+console.log('A');
+
+fs.readFile('syntax/sample.txt', 'utf8', function(err, result){
+  console.log(result);
+});
+console.log('C');
+
+//ACB 비동기적
